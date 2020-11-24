7faffe5d193c9bd0c6398587cca25e3d5dd8c526
antonio9520 <antonio.vidal95@hotmail.com>
2020-11-24 15:20:50 -0300

    arreglo auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const auth = require("../middleware/auth");
+const auth = require("../middleware/Auth");
 
 //login usuario
 //api/auth
